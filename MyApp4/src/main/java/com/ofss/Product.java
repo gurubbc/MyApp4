8ebb0ebe23c5f09b0d01c3b31b323bb2ff9b0b66
Guru <gurumurthy.ramamurthy@gmail.com>
2021-02-21 22:05:59 +0530

    commit 1

---
+++ MyApp4/src/main/java/com/ofss/Product.java
@@ -0,0 +1,15 @@
+package com.ofss;
+
+public class Product {
+	String n;
+	public Product(String n)
+	{
+		this.n=n;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
---
+++ MyApp4/src/main/java/com/ofss/TestProduct.java
@@ -0,0 +1,12 @@
+package com.ofss;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		Product p=new Product("chair");
+		System.out.println("Start");
+		System.out.println(p);
+
+	}
+
+}
